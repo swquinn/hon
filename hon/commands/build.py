abf5830be3489b2466df3ac212b93da1e1622ed1
Sean Quinn <swquinn@gmail.com>
2019-06-09 18:04:24 -0400

    Modify the arguments for the build command
    This brings Hon's build command more inline with GitBook.

--- hon/commands/build.py
+++ hon/commands/build.py
@@ -1,3 +1,20 @@
+"""
+    hon.commands.build
+    ~~~~~
+
+    hon serve [OPTIONS] [BOOK] [OUTPUT]
+
+    [BOOK]
+
+    TBW
+
+    [OUTPUT]
+
+    The ``output`` argument is the directory that the book will be written into
+    following generation. Relative paths are interpreted relative to the book's
+    root directory. If omitted, hon will use the build.build-dir from .honrc or
+    default to ./book.
+"""
 import click
 import os
 
@@ -5,19 +22,16 @@ from hon import current_app
 from ..cli import with_context
 
 @click.command('build', short_help='Builds a book from its markdown files')
-@click.argument('source_dir', default=None, required=False)
-@click.option('--dest-dir', '-d', 'dest_dir', default='book',
-    help=("Output directory for the book. Relative paths are interpreted "
-        "relative to the book's root directory. If omitted, hon will use the "
-        "build.build-dir from .honrc or default to ./book."))
+@click.argument('book', default=None, required=False)
+@click.argument('output', default='book', required=False)
 @with_context
-def build_command(source_dir, dest_dir):
-    if source_dir is None:
-        source_dir = os.getcwd()
+def build_command(book, output):
+    if book is None:
+        book = os.getcwd()
     
-    source_dir_abspath = os.path.abspath(source_dir)
-    if not os.path.isdir(source_dir_abspath):
-        raise FileNotFoundError(f'Unable to find directory: {source_dir_abspath}, does the directory exist?')
+    book_abspath = os.path.abspath(book)
+    if not os.path.isdir(book_abspath):
+        raise FileNotFoundError(f'Unable to find directory: {book_abspath}, does the directory exist?')
 
-    current_app.load_books(source_path=source_dir_abspath)
+    current_app.load_books(source_path=book_abspath)
     current_app.build()
