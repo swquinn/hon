c20fe31ad83560221853f885bd370fa2fcfae240
Sean Quinn <swquinn@gmail.com>
2019-08-12 17:07:16 -0400

    Remove support for Python 2.7

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='>=3.5',
     install_requires=[
         'blinker',
         'click>=7.0',
@@ -59,8 +59,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
