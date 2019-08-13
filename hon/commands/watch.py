7224dea239f1e6a46b3484c3f5defc6f1b70ed82
Sean Quinn <swquinn@gmail.com>
2019-08-13 07:41:46 -0400

    Add short_help descriptions to the commands

--- hon/commands/watch.py
+++ hon/commands/watch.py
@@ -1,7 +1,7 @@
 import click
 from ..cli import with_context
 
-@click.command('watch')
+@click.command('watch', short_help='Watch the book source for changes and rebuild')
 @with_context
 def watch_command(ctx=None):
     pass
