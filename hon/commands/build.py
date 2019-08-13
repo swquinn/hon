1fca43f11f2de60f4b16a0bb1fab520afffc4da1
Sean Quinn <swquinn@gmail.com>
2019-08-12 22:16:45 -0400

    Improve readability of build command
    And also remove a superfluous command that was added for testing.

--- hon/commands/build.py
+++ hon/commands/build.py
@@ -32,22 +32,24 @@ class BuildCommand(click.Command):
     a custom Command object and modify the params that are returned.
     """
     def get_params(self, ctx):
-        rv = super(BuildCommand, self).get_params(ctx)
+        rv = self.params
         with ctx.ensure_object(ScriptInfo).load_app().app_context():
             renderers = current_app.renderers
             rendering_options = []
             for r in renderers:
                 rendering_options.append(r.get_build_render_option())
             rv = rv + rendering_options
+        help_option = self.get_help_option(ctx)
+        if help_option is not None:
+            rv = rv + [help_option]
         return rv
 
 
 @click.command('build', short_help='Builds a book from its markdown files', cls=BuildCommand)
 @click.argument('book', default=None, required=False)
 @click.argument('output', default=None, required=False)
-@click.option('--test', 'test_opt', is_flag=True, default=True)
 @with_context
-def build_command(book, output, test_opt, **kwargs): #, include_epub=True, include_html=True, include_pdf=True):
+def build_command(book, output, **kwargs): #, include_epub=True, include_html=True, include_pdf=True):
     """
     """
     #: The enabled/disabled renderers for this run of the build command are
